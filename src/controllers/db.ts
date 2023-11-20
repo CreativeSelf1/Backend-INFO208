5fab2fa54310599efb32ba886d4ece631bd70130
CreativeSelf1 <fernacastillo.perez0607@gmail.com>
2023-11-20 20:25:10 -0300

    cambios

--- src/controllers/db.ts
+++ src/controllers/db.ts
@@ -1,5 +1,3 @@
-// db.ts
-
 import mysql from "mysql2/promise";
 
 const pool = mysql.createPool({
--- src/controllers/interactionController.ts
+++ src/controllers/interactionController.ts
@@ -3,7 +3,7 @@ import { pool } from "./db";
 
 export const getAllInteractions = async (req: Request, res: Response) => {
   try {
-    const results = await pool.query("SELECT nombre_prensa, count(*) FROM categoria GROUP BY nombre_prensa");
+    const results = await pool.query("SELECT url_principal FROM medios_de_prensa");
     res.json(results[0]);
   } catch (error) {
     console.error("Error en la consulta a la base de datos:", error);
