2b21a926c3849a4c018de6bff52f97926ec596c2
CreativeSelf1 <fernacastillo.perez0607@gmail.com>
2023-11-30 13:13:08 -0300

    f

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,20 @@
 import express from 'express';
 import cors from 'cors';
-
+import "dotenv/config";
 import { interactionRouter } from './routes/interactionRoute';
+
 const app = express();
+const port = process.env.DB_PORT;
 
 // Habilitar CORS para todas las rutas
 app.use(cors());
 app.use(express.json());
 // Resto de la configuración de tu aplicación...
 
-const port = 3000;
 
 //apliacion
 app.use("/api/command",interactionRouter);
 
 app.listen(port, () => {
-  console.log(`Servidor escuchando en http://localhost:${port}`);
+  console.log(`Servidor escuchando en http://192.168.43.9:${port}`);
 });
