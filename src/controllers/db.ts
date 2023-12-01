bd2bbbad32a606d9a1c11965f884eeb904f4be50
Fernando Castillo <109258213+CreativeSelf1@users.noreply.github.com>
2023-12-01 12:42:37 -0300

    Update db.ts

--- src/controllers/db.ts
+++ src/controllers/db.ts
@@ -5,16 +5,16 @@ const dbHost = process.env.DB_HOST;
 const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASSWORD;
 const dbDatabase = process.env.DB_DATABASE;
-
+const database_port = process.env.DB_BASE_PORT ? parseInt(process.env.DB_BASE_PORT, 10) : 3306;
 
 
 
 const pool = mysql.createPool({
-  host: "192.168.0.9",
-  port: 3307,
-  user: "root",
-  password: "fernando0607",
-  database: "info",
+  host: dbHost,
+  port: database_port,
+  user: dbUser,
+  password: dbPassword,
+  database: dbDatabase,
   connectionLimit: 10,
 });
 
